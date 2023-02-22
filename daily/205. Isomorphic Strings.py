3f1d850f68fbb9159c8dcf8ce354ae8aa4f12655
Abel Abate <61546411+abelaba@users.noreply.github.com>
2023-02-22 21:59:14 +0300

    Create 205. Isomorphic Strings.py

---
+++ daily/205. Isomorphic Strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def check(self, s, t):
+        letterMapping = {}
+        stack = []
+        for i in range(len(s)):
+            if t[i] not in letterMapping:
+                stack.append(s[i])
+                letterMapping[t[i]] = s[i]
+            else:
+                stack.append(letterMapping[t[i]])
+        return ''.join(stack) == s
+    
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return self.check(s, t) and self.check(t, s)
+        
